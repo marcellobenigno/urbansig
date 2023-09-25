e44810b543bddfdbb7617931bb25b0749ed90a85
Marcello Benigno <benigno.marcello@gmail.com>
2023-09-25 17:37:31 -0300

    ajusta o form

--- urbansig/cadastro/forms.py
+++ urbansig/cadastro/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core.exceptions import ValidationError
 from leaflet.forms.widgets import LeafletWidget
 
 from . import models
@@ -8,13 +9,36 @@ class LoteMapWidget(LeafletWidget):
     geometry_field_class = 'LoteGeometryField'
 
 
+def get_quadra(geom):
+    return models.Quadra.objects.filter(
+        geom__intersects=geom
+    ).first()
+
+
 class LoteForm(forms.ModelForm):
+
+    def clean_geom(self):
+        lote_geom = self.cleaned_data.get('geom')
+
+        if not get_quadra(lote_geom):
+            raise ValidationError(
+                'O lote deve ser criado dentro de uma quadra!'
+            )
+
+        return lote_geom
+
+    def save(self, commit=True):
+        lote = super().save(commit=False)
+        lote.quadra = get_quadra(self.cleaned_data.get('geom'))
+        if commit:
+            lote.save()
+        return lote
+
     class Meta:
         model = models.Lote
         fields = [
             'num_lote',
             'situacao',
-            'quadra',
             'geom',
         ]
         widgets = {'geom': LoteMapWidget()}
